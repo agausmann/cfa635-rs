25b99ba3ee2aecd393ea2406a72fb81af7957810
Adam Gausmann <agausmann@fastmail.com>
2021-12-27 13:43:06 -0600

    Add blank example

---
+++ examples/blank.rs
@@ -0,0 +1,16 @@
+use anyhow::Context;
+use cfa635::Device;
+use std::env::args;
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let mut args = args();
+    let path = args.nth(1).context(USAGE)?;
+    let mut device = Device::new(path)?;
+    device.set_backlight(0, 0)?;
+
+    Ok(())
+}
+
+const USAGE: &str = "usage: [port]";
